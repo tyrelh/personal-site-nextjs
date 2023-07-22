69287f98a1a34e59571b20043c0b031a580aac88
tyrelh <tyrel.hiebert@gmail.com>
2023-07-22 11:53:46 -0700

    add return to homepage link in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,22 +1,27 @@
 import Anchor from "../elements/Anchor";
 import { ThunderboltFilled, TwitterOutlined, GithubFilled } from "@ant-design/icons";
-import { Layout } from "antd"
+import { Layout, Space } from "antd"
 import LinkedinLogoIcon from "../elements/LinkedinLogoIcon";
+import Link from "next/link";
 const { Footer: AntFooter } = Layout 
 
 export default function Footer() {
   return (
     <AntFooter>
+      <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+        <p>
+          <Link href="/">Return to homepage</Link>
+        </p>
         <p>
           Made with <ThunderboltFilled /> by Tyrel Hiebert   <Anchor href="https://twitter.com/tyreldelaney"><TwitterOutlined /></Anchor>  <Anchor href="https://github.com/tyrelh"><GithubFilled /></Anchor>  <Anchor href="https://www.linkedin.com/in/tyrelhiebert/"><LinkedinLogoIcon /></Anchor><br/>
-          {/* This site uses <Anchor href="https://plausible.io/">Plausible</Anchor> to collect privacy mindful visitor stats<br/> */}
         </p>
-        <p>
-          I use <Anchor href="https://plausible.io/superflux.dev">Plausible.io</Anchor> to collect privacy respecting visitor analytics.
-        </p>
-        <p>
-          <Anchor href="https://www.github.com/tyrelh/personal-site-nextjs/"><GithubFilled /> View website source</Anchor>
-        </p>
-      </AntFooter>
-  )
+      </Space>
+      <p>
+        I use <Anchor href="https://plausible.io/superflux.dev">Plausible.io</Anchor> to collect privacy respecting visitor analytics.
+      </p>
+      <p>
+        <Anchor href="https://www.github.com/tyrelh/personal-site-nextjs/"><GithubFilled /> View website source</Anchor>
+      </p>
+    </AntFooter>
+)
 }
