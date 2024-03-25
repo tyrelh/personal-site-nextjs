6ae70a9a1d03445d03ee0dc591c4f2fdc019e377
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-03-25 10:36:17 -0700

    article list items use an onClick handler for navi instead of <a>

--- components/elements/ArticlePreviewList.tsx
+++ components/elements/ArticlePreviewList.tsx
@@ -3,6 +3,7 @@ import { CalendarOutlined, ReadOutlined } from "@ant-design/icons"
 import { PostData, PostMetadata } from "../../dtos/PostData"
 import Link from "next/link"
 import Image from "next/image"
+import { useRouter } from 'next/navigation'
 import Anchor from "./Anchor"
 import Hashtag from "./Hashtag"
 
@@ -12,7 +13,12 @@ export interface Props {
 }
 
 export default function ArticlePreviewList(props: Props) {
-const { articleMetadataList } = props
+  const { articleMetadataList } = props
+  const router = useRouter()
+
+  const navigateToArticle = (slug: string) => {
+    router.push(`/blog/${slug}`)
+  }
   
   return (
     <List
@@ -21,38 +27,37 @@ const { articleMetadataList } = props
       split={false}
       dataSource={articleMetadataList}
       renderItem={ (post: PostMetadata | PostData) => (
-        // <Anchor href={`/blog/${post.slug}`}>
-          <List.Item
-            key={post.slug}
-            extra={
-              <img
-                className="article-preview-image" 
-                alt=""
-                src={post?.hero ? post.hero : null}/>
-            }
-          >
-            <h3 className="article-preview-title">
-              { post?.title ? post.title : post?.slug }
-            </h3>
-            {
-              post?.date &&
-                <h4>
-                  <CalendarOutlined /> {post.date}  <ReadOutlined className="readtime-icon" /> {post.readTimeInMinutes} Minute Read
-                </h4>
-            }
-            <p className="article-preview-excerpt">
-              {post?.excerpt ? post.excerpt : null}
+        <List.Item
+          key={post.slug}
+          extra={
+            <img
+              className="article-preview-image" 
+              alt=""
+              src={post?.hero ? post.hero : null}/>
+          }
+          onClick={() => navigateToArticle(post.slug)}
+        >
+          <h3 className="article-preview-title">
+            { post?.title ? post.title : post?.slug }
+          </h3>
+          {
+            post?.date &&
+              <h4>
+                <CalendarOutlined /> {post.date}  <ReadOutlined className="readtime-icon" /> {post.readTimeInMinutes} Minute Read
+              </h4>
+          }
+          <p className="article-preview-excerpt">
+            {post?.excerpt ? post.excerpt : null}
+          </p>
+          {
+            post?.tags?.length &&
+            <p>
+              {post.tags.map((tag: string) => (
+                <Hashtag key={tag} tag={tag}/>
+              ))}
             </p>
-            {
-              post?.tags?.length &&
-              <p>
-                {post.tags.map((tag: string) => (
-                  <Hashtag key={tag} tag={tag}/>
-                ))}
-              </p>
-            }
-          </List.Item>
-        // </Anchor>
+          }
+        </List.Item>
       )}
     />
   )
