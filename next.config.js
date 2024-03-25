cef2b8ba70af99518debf07eecdd211aa31aa617
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-03-24 21:17:29 -0700

    updated everything to latest 🤯

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: ''
+  assetPrefix: '',
+  transpilePackages: [
+    "antd",
+    "rc-util",
+    "@babel/runtime",
+    "@ant-design/icons",
+    "@ant-design/icons-svg",
+    "rc-pagination",
+    "rc-picker",
+    "rc-tree",
+    "rc-table",
+  ]
 }
 
 module.exports = nextConfig
