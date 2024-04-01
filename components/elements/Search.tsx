cd3d8df806b77f1ad9d40b5eff43626c0602c0b4
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-03-31 22:42:02 -0700

    clean up and simplification

--- components/elements/Search.tsx
+++ components/elements/Search.tsx
@@ -1,86 +1,53 @@
-import React, { PropsWithChildren, useEffect, useState, useMemo, useRef } from "react";
-import { Input, Select, SelectProps, Spin, Affix } from 'antd';
-import type { SearchProps } from 'antd/es/input/Search';
+import React, { useEffect, useState, useRef } from "react";
+import { Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import debounce from 'lodash/debounce';
 import { useRouter } from 'next/navigation'
-
-
-const { Search } = Input;
-
-export interface Props extends PropsWithChildren<any> {
-
-}
 
 interface OptionValue {
     label: string;
     value: string;
-  }
+}
 
-
-export default function SearchInput(props: Props) {
-    const router = useRouter()
+export default function SearchInput() {
+    const router = useRouter();
     const [searchIndex, setSearchIndex] = useState<Map<string, object>>(new Map());
     const [searchIndexLoaded, setSearchIndexLoaded] = useState<boolean>(false);
-    const [options, setOptions] = useState<{ value: string, label: string }[]>([]);
+    const [options, setOptions] = useState<OptionValue[]>([]);
     const [previousSearchTerm, setPreviousSearchTerm] = useState<string>('');
-    const [fetching, setFetching] = useState<boolean>(false);
     const fetchRef = useRef(0);
     const [isMobile, setIsMobile] = useState<boolean>(false);
-    
 
-    const navigateToArticle = (slug: string) => {
-        router.push(`/blog/${slug}`)
+    const navigateToArticle = (value: string) => {
+        router.push(`/blog/${value}`);
     }
 
-    const onChange = useMemo(() => {
-        const getItems = (searchTerm: string) => {
-            fetchRef.current += 1;
-            const currentFetch = fetchRef.current;
-            setFetching(true);
-            console.log('getItems', searchTerm);
-            if (currentFetch !== fetchRef.current) {
-                console.log('fetch cancelled');
-                return;
-            }
-            if (searchTerm.length < 1) {
-                console.log('no search term');
-                setOptions([]);
-                setFetching(false);
-                return;
-            }
-            searchTerm = searchTerm.toLowerCase();
-            if (searchTerm === previousSearchTerm) {
-                return;
-            }
-            setPreviousSearchTerm(searchTerm);
-            if (searchIndex.entries() && searchIndex.has(searchTerm)) {
-                const newOptions = new Array<OptionValue>();
-                for (const item of searchIndex.get(searchTerm) as { title: string, slug: string }[]) {
-                    newOptions.push({
-                        value: item.slug,
-                        label: item.title,
-                    });
-                }
-                console.log('newOptions', newOptions.length);
-                setOptions(newOptions);
-            } else {
-                console.log('no results')
-                setOptions([]);
-            }
-            setFetching(false);
+    const onChange = (searchTerm: string) => {
+        fetchRef.current += 1;
+        const currentFetch = fetchRef.current;
+        if (currentFetch !== fetchRef.current) {
+            return;
         }
-        if (!fetching) {
-            return debounce(getItems, 50);
+        if (searchTerm.length < 1) {
+            setOptions([]);
+            return;
+        }
+        searchTerm = searchTerm.toLowerCase();
+        if (searchTerm === previousSearchTerm) {
+            return;
+        }
+        setPreviousSearchTerm(searchTerm);
+        if (searchIndex.entries() && searchIndex.has(searchTerm)) {
+            const newOptions = new Array<OptionValue>();
+            for (const item of searchIndex.get(searchTerm) as { title: string, slug: string }[]) {
+                newOptions.push({
+                    value: item.slug,
+                    label: item.title,
+                });
+            }
+            setOptions(newOptions);
         } else {
-            console.log('fetching');
+            setOptions([]);
         }
-    }, [searchIndex])
-
-    const onSelect = (value: string, option: object) => {
-        console.log('onSelect', value, option);
-        navigateToArticle(value);
-    
     }
 
     useEffect(() => {
@@ -94,21 +61,13 @@ export default function SearchInput(props: Props) {
         });
 
         const handleResize = () => {
-            console.log('resize', isMobile);
-            // console.log('resize', window.matchMedia('(max-width: 768px)').matches);
             setIsMobile(window.matchMedia('(max-width: 768px)').matches);
         };
     
-        // Initial check
         handleResize();
-    
-        // Listen for window resize events
         window.addEventListener('resize', handleResize);
     
-        // Cleanup
-        
-
-
+        // load search index
         if (!searchIndexLoaded) {
             import("./search-index.json")
                 .then((data) => {
@@ -139,20 +98,18 @@ export default function SearchInput(props: Props) {
         >
             <Select
                 popupClassName="search-items"
-                // style={{ width: isMobile ? 200 : 300}}
                 style={{ width: 300}}
                 showSearch
                 filterOption={false}
                 onSearch={onChange}
-                onSelect={onSelect}
+                onSelect={navigateToArticle}
                 placeholder={searchIndexLoaded ? "Search" : '...'}
-                optionFilterProp="children"
                 suffixIcon={<SearchOutlined />}
-                filterSort={(optionA: OptionValue, optionB: OptionValue) =>
-                    (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                filterSort={(a: OptionValue, b: OptionValue) =>
+                    (a?.label ?? '').toLowerCase().localeCompare((b?.label ?? '').toLowerCase())
                 }
                 options={options}
-                notFoundContent={fetching ? <Spin size="small" /> : null}
+                notFoundContent={null}
             />
         </div>
     );
